5227bd13c917ea92f4a8cf4b45d0adbe584bdb77
David Joly <dj06700@gmail.com>
2024-06-17 12:02:09 +0200

    debugage pour changements css au clique filtre et de la navigation des fonctions prvImage et nextImage

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,8 +153,9 @@
           index = i ;
         }
       });
+      //index-1 pour passer à l'image précédente
       next =
-        imagesCollection[index] ||
+        imagesCollection[index -1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +193,8 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      //index+1 pour passer à l'image suivante
+      next = imagesCollection[index +1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
